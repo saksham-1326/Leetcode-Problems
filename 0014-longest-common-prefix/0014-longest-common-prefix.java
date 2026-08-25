4b6d8ad45e9b680cbb38925f644688c33ee1c795
Saksham <saksham.132611@gmail.com>
2026-08-25 15:55:53 +0530

    Time: 1 ms (60.9%), Space: 43.3 MB (50.74%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String longestCommonPrefix(String[] str) {
+        Arrays.sort(str);
+        String s1=str[0];
+        String s2=str[str.length-1];
+
+            int idx=0;
+
+            while(idx<s1.length() && idx<s2.length())
+            {
+                if(s1.charAt(idx)==s2.charAt(idx))
+            {
+                idx++;
+            }
+            else
+            {
+                break;
+            }
+        }
+            return s1.substring(0,idx);
+        
+    }
+}
